b6e2c717b7cf8befee0e8877ff370aeb9ecd7240
Oly <oliver.marks@influentialsoftware.com>
2017-03-29 09:04:27 +0100

    added emacs shortcuts aliases of emax smax and gmax

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -3,3 +3,13 @@
 set -x GOPATH /home/oly/go
 set -x GO15VENDOREXPERIMENT 1
 set PATH $GOPATH/bin $PATH
+
+set -x ALTERNATE_EDITOR ""
+set -x EDITOR "emacsclient -t"                  # $EDITOR should open in terminal
+set -x VISUAL "emacsclient -c -a emacs"         # $VISUAL opens in GUI with non-daemon as alternate
+
+alias emax="emacsclient -t"                      # used to be "emacs -nw"
+alias smax="sudo emacsclient -t"                # used to be "sudo emacs -nw"
+alias gmax="emacsclient -c -a emacs"             # new - opens the GUI with alternate non-daemon
+
+
